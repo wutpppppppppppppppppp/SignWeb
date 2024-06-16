c3e510da9d90c2f66a07fecc5142a8ca929cf75d
wutpppppppppppppppppp <gansahy@hotmail.com>
2024-06-16 15:22:55 +0700

    add: almost all available services except listener ( used in custom livestreaming in studio )

---
+++ src/services/trackerService.js
@@ -0,0 +1,22 @@
+// src/services/trackerService.js
+import axios from 'axios'
+
+export const tracker = async (config) => {
+  try {
+    const { ip_address, port, api_key } = config
+
+    console.log(`http://${ip_address}:${port}/v2/${api_key}/tracker`)
+    const response = await axios.get(`http://${ip_address}:${port}/v2/${api_key}/tracker`, {
+      device_id: '',
+      bone_attached: '', // hips
+      position: { X: 1.0, Y: 1.0, Z: 1.0 },
+      rotation: { X: 1.0, Y: 1.0, Z: 1.0 },
+      timeout: 2.0,
+      is_query_only: true,
+    })
+    return response.data
+  } catch (error) {
+    console.log(error)
+    return error
+  }
+}

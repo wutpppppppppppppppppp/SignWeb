9141feb2ba4a627b265059a39d97d883d5ea369a
wutpppppppppppppppppp <gansahy@hotmail.com>
2024-06-16 15:34:13 +0700

    change: write calibrateService.js

--- src/services/calibrateService.js
+++ src/services/calibrateService.js
@@ -1,45 +1,27 @@
-// src/calibrateService.js
+// src/services/calibrateService.js
 import axios from 'axios'
 
-const fetchCalibrateData = async () => {
+export const calibrate = async (config) => {
   try {
-    const response = await axios.get('/calibrateData.json')
-    return response.data
-  } catch (error) {
-    console.error('Error fetching calibrate data', error)
-    throw error
-  }
-}
+    const { ip_address, port, api_key, SMARTSUIT_NAME } = config
 
-export const calibrate = async () => {
-  try {
-    const data = {
-      ip_address: '172.20.10.3',
-      port: '14053',
-      api_key: '1234',
-      frame_rate: '60',
-      back_to_live: false,
-    }
-    const { ip_address, port, api_key, frame_rate, back_to_live } = data
     console.log(`http://${ip_address}:${port}/v1/${api_key}/calibrate`)
 
-    json = {
-    'device_id': SMARTSUIT_NAME,
-    'countdown_delay': 30,
-    'skip_suit' : True,
-    'skip_gloves' : True,
-    'use_custom_pose' : False,
-    'pose' : 'straight-arms-down'
-    } 
-
     const response = await axios.post(`http://${ip_address}:${port}/v1/${api_key}/calibrate`, {
-      frame_rate: frame_rate,
-      back_to_live: back_to_live,
+      device_id: SMARTSUIT_NAME,
+      countdown_delay: 30,
+      skip_suit: true,
+      skip_gloves: true,
+      use_custom_pose: false,
+      pose: 'straight-arms-down',
     })
+
     console.log(`response: ${response}`)
+
     return response.data
   } catch (error) {
     console.error('Error making API request', error)
     throw error
   }
 }
+

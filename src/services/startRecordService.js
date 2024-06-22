3d25875642da5e0cdc0566d40458ca7a8ad60745
wutpppppppppppppppppp <gansahy@hotmail.com>
2024-06-22 16:33:53 +0700

    fix: button API weird Stuff

--- src/services/startRecordService.js
+++ src/services/startRecordService.js
@@ -3,16 +3,21 @@ import axios from "axios"
 
 export const startRecording = async (config) => {
   try {
-    const { ip_address, port, api_key, CLIP_NAME,TIME_CODE,FRAME_RATE } = config
+    const {
+      ip_address,
+      port,
+      api_key,
+      // CLIP_NAME, TIME_CODE, FRAME_RATE,
+    } = config
 
-    console.log(`http://${ip_address}:${port}/v1/${api_key}/recording/start`)
+    console.log(`http://${ip_address}:${port}/v2/${api_key}/recording/start`)
 
     const response = await axios.post(
       `http://${ip_address}:${port}/v1/${api_key}/recording/start`,
       {
-        filename: CLIP_NAME,
-        time: TIME_CODE,
-        frame_rate: FRAME_RATE
+        // filename: CLIP_NAME,
+        // time: TIME_CODE,
+        // frame_rate: FRAME_RATE
       }
     )
 
--- src/services/stopRecordService.js
+++ src/services/stopRecordService.js
@@ -3,16 +3,21 @@ import axios from "axios"
 
 export const stopRecording = async (config) => {
   try {
-    const { ip_address, port, api_key, CLIP_NAME,TIME_CODE,FRAME_RATE,BACK_TO_LIVE } = config
+    const {
+      ip_address,
+      port,
+      api_key,
+      // CLIP_NAME,TIME_CODE,FRAME_RATE,BACK_TO_LIVE
+    } = config
 
     console.log(`http://${ip_address}:${port}/v2/${api_key}/recording/stop`)
     const response = await axios.post(
       `http://${ip_address}:${port}/v2/${api_key}/recording/stop`,
       {
-        filename: CLIP_NAME,
-        time: TIME_CODE,
-        frame_rate: FRAME_RATE,
-        back_to_live: BACK_TO_LIVE
+        // filename: CLIP_NAME,
+        // time: TIME_CODE,
+        // frame_rate: FRAME_RATE,
+        // back_to_live: BACK_TO_LIVE
       }
     )
 

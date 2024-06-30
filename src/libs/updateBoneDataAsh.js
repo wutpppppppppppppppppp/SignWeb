c6f17b1113083c938280c3a89796103292e66f5d
wutpppppppppppppppppp <gansahy@hotmail.com>
2024-06-30 22:14:01 +0700

    fix: chest fixing

--- src/libs/updateBoneDataAsh.js
+++ src/libs/updateBoneDataAsh.js
@@ -1,46 +1,31 @@
 import { mappedPart } from "./mappedPart"
 import { nodeTraverse } from "./nodeTraverse"
+import { addAxesHelper } from "./axesHelper"
 import * as THREE from "three"
 
 export function updateBoneData(jsonData, model) {
   model = nodeTraverse(model, "Hips")
   model.traverse((node) => {
-    // console.log(JSON.stringify(node.name))
-    // console.log(jsonData.scene.actors[0].body, mappedName)
-
+    // console.log(JSON.stringify(node.name),jsonData.scene.actors[0].body, mappedName)
     let mappedName = mappedPart(node.name)
     if (mappedName && jsonData.scene.actors[0].body[mappedName]) {
-      // Adding AxesHelper to the bone (only once)
-      //   if (!node.getObjectByName("axesHelper")) `1{
-      //     const axesHelper = new THREE.AxesHelper(0.1)
-      //     axesHelper.name = "axesHelper"
-      //     node.add(axesHelper)
-      //   }
-
-      // Access the jsonData for assigning
       const boneData = jsonData.scene.actors[0].body[mappedName]
-      // Construct Vector3 objects
       const desiredWorldPosition = new THREE.Vector3(
         boneData.position.x,
         boneData.position.y,
         boneData.position.z
       )
-      // Create a variable to collect the parent position
-      const parentWorldMatrix = new THREE.Matrix4()
-      // Tell the node that we will update the relative position
       node.parent.updateMatrixWorld(true)
-
+      const parentWorldMatrix = new THREE.Matrix4()
       parentWorldMatrix.copy(node.parent.matrixWorld)
-
       const inverseParentWorldMatrix = new THREE.Matrix4()
         .copy(parentWorldMatrix)
         .invert()
-
+      // multiply 3D vector with inverse parent world matrix
       const desiredLocalPosition = desiredWorldPosition.applyMatrix4(
         inverseParentWorldMatrix
       )
       node.position.copy(desiredLocalPosition)
-
       const desiredWorldQuaternion = new THREE.Quaternion(
         boneData.rotation.x,
         boneData.rotation.y,
@@ -57,10 +42,49 @@ export function updateBoneData(jsonData, model) {
         inverseParentWorldQuaternion
       )
       node.quaternion.copy(desiredLocalQuaternion)
-
       node.updateMatrixWorld(true)
+    } else if (mappedName == null) {
+      // if (node.parent) {
+      //   node.position.copy(node.parent.position)
+      //   node.quaternion.copy(node.parent.quaternion)
+      //   node.updateMatrixWorld(true)
+      // }
+      // There is no such bone in the JSON
+      // const desiredWorldPosition = new THREE.Vector3(
+      //   node.position.x,
+      //   node.position.y,
+      //   node.position.z
+      // )
+      // node.parent.updateMatrixWorld(true)
+      // const parentWorldMatrix = new THREE.Matrix4()
+      // parentWorldMatrix.copy(node.parent.matrixWorld)
+      // const inverseParentWorldMatrix = new THREE.Matrix4()
+      //   .copy(parentWorldMatrix)
+      //   .invert()
+      // // multiply 3D vector with inverse parent world matrix
+      // const desiredLocalPosition = desiredWorldPosition.applyMatrix4(
+      //   inverseParentWorldMatrix
+      // )
+      // node.position.copy(desiredLocalPosition)
+      // const desiredWorldQuaternion = new THREE.Quaternion(
+      //   node.rotation.x,
+      //   node.rotation.y,
+      //   node.rotation.z,
+      //   node.rotation.w
+      // )
+      // const parentWorldQuaternion = new THREE.Quaternion()
+      // node.parent.updateMatrixWorld(true)
+      // parentWorldQuaternion.copy(node.parent.quaternion)
+      // const inverseParentWorldQuaternion = parentWorldQuaternion
+      //   .clone()
+      //   .invert()
+      // const desiredLocalQuaternion = desiredWorldQuaternion.premultiply(
+      //   inverseParentWorldQuaternion
+      // )
+      // node.quaternion.copy(desiredLocalQuaternion)
+      // node.updateMatrixWorld(true)
     } else {
-      // console.error(`No data found for bone: ${node.name}`)
+      console.error(`Unsupported bone: ${node.name}`)
     }
     // }
   })

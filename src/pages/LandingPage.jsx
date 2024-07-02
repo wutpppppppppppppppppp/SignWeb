1357fcf25299cfc217d98e92a225772ba87117e5
wutpppppppppppppppppp <gansahy@gmail.com>
2024-07-03 00:42:53 +0700

    add: landing page is done!

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,32 +1,33 @@
-import * as React from "react"
-import Navbar from "../components/navbar"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { FaUser } from "react-icons/fa6";
+import PathConstants from "../routes/pathConstants";
 
 const LandingPage = () => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <div className="navbar">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <Link to={PathConstants.INFO_POLICY} className="btn btn-ghost text-xl">บลูมบีท</Link>
         </div>
         <div className="flex-none">
-          <button className="btn btn-square btn-ghost text-primary-content">
-            <FontAwesomeIcon icon="fa-solid fa-user" className=""/>
-          </button>
+          <Link to={PathConstants.LOGIN} className="btn btn-outline btn-ghost text-primary">
+            <FaUser />เข้าสู่ระบบ
+          </Link>
         </div>
       </div>
-      <div className="hero min-h-screen">
+      <div className="hero h-screen">
         <div className="hero-content text-center">
           <div className="max-w-fit">
             <p className="text-8xl mb-8">คลังเก็บภาษามือไทย</p>
-            <button className="btn btn-primary text-primary-content">
+            <Link to={PathConstants.CATEGORY} className="btn btn-primary text-primary-content">
               ค้นหาภาษามือ
-            </button>
+            </Link>
           </div>
         </div>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default LandingPage
+export default LandingPage;

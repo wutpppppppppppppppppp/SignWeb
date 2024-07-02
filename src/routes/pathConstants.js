523724ef0cb99024c5594fc3da91a497f2c2e19a
wutpppppppppppppppppp <gansahy@hotmail.com>
2024-07-02 13:33:46 +0700

    add: pathConstants, pathroutes

--- src/routes/pathConstants.js
+++ src/routes/pathConstants.js
@@ -1,4 +1,19 @@
 const PathConstants = {
-  LANDGING: "/",
-  ABOUT: "/about",
+  // Public pages
+  LANDING: "/",
+  INFO_POLICY: "/info-policy",
+  LOGIN: "/login",
+  SIGN_IN: "/sign-in",
+
+  // Pages requiring login
+  CATEGORY: "/categoryฝ๕",
+  VOCABULARY: "/vocabulary",
+  DISPLAY_VOCAB: "/display-vocab",
+
+  SETUP_FORM: "/setup-form",
+  CONTROL_BUTTONS: "/control-buttons",
+  THREE_SCENE: "/three-scene",
+  RECORD: "/record",
 }
+
+export default PathConstants

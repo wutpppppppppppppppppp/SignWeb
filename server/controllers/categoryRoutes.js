a4d83bbeedd3d9737e908c0dd80ed98772b49b73
wutpppppppppppppppppp <gansahy@gmail.com>
2024-07-04 19:49:33 +0700

    add: Mongo Atlas connected

--- server/controllers/categoryRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getCategories, createCategory } = require('./categoryController');
-const router = express.Router();
-
-router.get('/', getCategories);
-router.post('/', createCategory);
-
-module.exports = router;

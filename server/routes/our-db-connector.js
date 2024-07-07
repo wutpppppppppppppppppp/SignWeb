0c8df9ec11697b9179ae215e4b2523795ac6ef4f
wutpppppppppppppppppp <gansahy@gmail.com>
2024-07-07 19:16:19 +0700

    fix: only one folder server hahahahha

---
+++ server/routes/our-db-connector.js
@@ -0,0 +1,17 @@
+// ESM
+import fastifyPlugin from "fastify-plugin";
+import fastifyMongo from "@fastify/mongodb";
+
+/**
+ * @param {FastifyInstance} fastify
+ * @param {Object} options
+ */
+async function dbConnector(fastify, options) {
+  fastify.register(fastifyMongo, {
+    url: "mongodb://localhost:27017/test_database",
+  });
+}
+
+// Wrapping a plugin function with fastify-plugin exposes the decorators
+// and hooks, declared inside the plugin to the parent scope.
+export default fastifyPlugin(dbConnector);

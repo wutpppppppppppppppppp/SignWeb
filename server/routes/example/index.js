d0dac6656d56ac2e15a7eb574d0906fa3eaeb300
wutpppppppppppppppppp <gansahy@hotmail.com>
2024-07-09 00:46:04 +0700

    Co-authored-by: TGRZiminiar <TGRZiminiar@users.noreply.github.com>

--- server/routes/example/index.js
+++ server/routes/example/index.js
@@ -1,19 +1,26 @@
 // Import the framework and instantiate it
-import Fastify from 'fastify'
+import Fastify from "fastify";
 const fastify = Fastify({
-  logger: true
-})
+  logger: true,
+});
 
 // Declare a route
-fastify.get('/', async function handler (request, reply) {
-  return { hello: 'world' }
-})
+fastify.get("/", async function handler(request, reply) {
+  return { hello: "world" };
+}, );
+
+fastify.get("/test", async function temp(request, reply, done) {
+    
+  done()
+},  async function handler(request, reply) {
+      return { hello: "world" };
+}, )
 
 // Run the server!
-fastify.listen({ port: 3000, host: '0.0.0.0' }, function (err, address) {
+fastify.listen({ port: 3000, host: "0.0.0.0" }, function (err, address) {
   if (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
-  fastify.log.info(`server listening on ${address}`)
-})
+  fastify.log.info(`server listening on ${address}`);
+});

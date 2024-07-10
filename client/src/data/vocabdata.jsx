20425aa035b3fc9b6519e562ca75b8667800f9e2
NANAYEMYINTKYI <ayemyint.kyi2004@gmail.com>
2024-07-10 13:28:34 +0700

    Add lazy loading for vocab image and add vocabdata file

---
+++ client/src/data/vocabdata.jsx
@@ -0,0 +1,13 @@
+const vocabularies = [
+    { name: "ไข่เจียว", image: "https://s359.kapook.com/r/600/auto/pagebuilder/201d6cc5-020e-466b-983e-4c7dea7f5dfa.jpg" },
+    { name: "ต้มยำกุ้ง", image: "https://lh3.googleusercontent.com/proxy/4Fa81R1g1szKWWttMYMmlKUgd8TAEC29j5jtcQ-qbA_Hq-3F72UTynts9BT3Fdq3-HdYL7_HjChORSwo-N8dyvk685D1xS1H1n3fC1TMKTR2CflBY-snUAnKgpPF" },
+    { name: "ผัดไท", image: "https://img.kapook.com/u/2015/surauch/cook2/PT1.jpg" },
+    { name: "เกี๊ยวทอด", image: "https://s359.kapook.com/pagebuilder/42ce18d3-1c13-4d6f-a03f-9964cf57124c.jpg" },
+];
+
+const vocabDescriptions = {
+    "ไข่เจียว": "ไข่ไก่ที่ตีให้เข้ากันก่อนทอด",
+    "กล้วย": "สีเหลือง อร่อย",
+};
+
+export { vocabularies, vocabDescriptions };
--- client/src/pages/DisplayVocabAdmin.jsx
+++ client/src/pages/DisplayVocabAdmin.jsx
@@ -2,9 +2,22 @@
 import { useParams } from "react-router-dom";
 import Navbar3 from "../components/Navbar3";
 import ThreeScene2 from "../pages/ThreeScene2";
+import { useEffect, useState } from "react";
 
 const DisplayVocab = () => {
   const { categoryName, vocabName } = useParams();
+  const [description, setDescription] = useState("");
+
+  const vocabDescriptions = {
+    ไข่เจียว : "ลูกกลม มีสีแดง เขียว รสเปรี้ยว",
+    กล้วย: "สีเหลือง อร่อย",
+  };
+
+  useEffect(() => {
+    console.log(vocabName)
+    setDescription(vocabDescriptions[vocabName] || "ไม่พบคำอธิบาย");
+  },[vocabName]);
+
   return (
     <div className="w-screen h-screen flex flex-col relative">
       <Navbar3 title={`วิดีโอภาษามือ : ${vocabName}`} />
@@ -17,8 +30,8 @@ const DisplayVocab = () => {
             <div className="card-body relative">
               <h3 className="card-title font-bold text-2xl">{vocabName}</h3>
               <img src="https://static.libertyprim.com/files/familles/pomme-large.jpg?1569271834" alt="Apple" className="flex mx-auto w-2/4" />
-              <a className="category text-xl">ประเภทคำ : {vocabName}</a>
-              <a className="explanation text-xl">คำอธิบาย : ลูกกลม มีสีแดง เขียว รสเปรี้ยว</a>
+              <a className="category text-xl">ประเภทคำ : {categoryName}</a>
+              <a className="explanation text-xl">คำอธิบาย : {description}</a>
               <a className="approve text-xl">รับรองโดย : คุณน่ารัก ครุครุคริคริ</a>
               <div className="absolute inset-x-0 bottom-0 p-4 bg-white shadow-lg flex justify-between">
                 <button className="btn bg-others text-white w-1/2 text-center">ดูคำอื่น</button>

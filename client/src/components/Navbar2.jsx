0f70b20c3b7f3992fc40f8d8f1433118c00a00bb
ppearyxx <pearp.pinnuwat@gmail.com>
2024-07-11 16:41:32 +0700

    Edit : Center Navbar Text

--- client/src/components/Navbar2.jsx
+++ client/src/components/Navbar2.jsx
@@ -6,17 +6,17 @@ import { Link } from "react-router-dom";
 function Navbar({ title }) {
   return (
     <div className="navbar bg-primary text-primary-content p-1">
-
-      <div className="navbar-start">
+      <div className="navbar-start w-1/3">
         <Link to="/" className="btn btn-ghost">
           <IoIosArrowBack className="size-6" title="กลับไปหน้าก่อน"/>
         </Link>
       </div>
 
-      <div className="navbar-center">
+      <div className="navbar-center w-1/3 flex justify-center">
         <span className="text-xl font-bold">{title}</span>
       </div>
       
+      <div className="navbar-end w-1/3"></div>
     </div>
   );
 }

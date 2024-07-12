bf1f1ed007ecba64cdc2173d9716c810181bda58
ppearyxx <pearp.pinnuwat@gmail.com>
2024-07-12 19:05:16 +0700

    Modify : Frontend Page

--- client/src/pages/Category.jsx
+++ client/src/pages/Category.jsx
@@ -1,73 +1,43 @@
-import React, { useState } from "react"
-import { Link } from "react-router-dom"
-import CatCard from "../components/CatCard"
-import Pagination from "../components/Pagination"
-import Navbar from "../components/navbar"
-import placeHolder from "../assets/placeholder.png"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import CatCard from "../components/CatCard";
+import Pagination from "../components/Pagination";
+import Navbar from "../components/Navbar";
+import placeHolder from "../assets/placeholder.png";
 
 const allCategories = [
-  {
-    name: "อาหาร",
-    image:
-      "https://as1.ftcdn.net/v2/jpg/01/92/00/78/1000_F_192007831_OGdxh37OAqmJpoMuWfgbKKYaQgpa9SJN.jpg",
-  },
-  {
-    name: "สัตว์",
-    image:
-      "https://www.timeforkids.com/wp-content/uploads/2023/11/G3G5_231117_bear_steps.jpg?w=1024",
-  },
-  {
-    name: "สี",
-    image:
-      "https://hips.hearstapps.com/hmg-prod/images/gettyimages-1270527025-6515e6930b257.jpg?crop=1xw:0.84375xh;center,top",
-  },
-  { name: "ร่างกาย", image: placeHolder },
-  {
-    name: "ครอบครัว",
-    image:
-      "https://xcdn-cf.vuihoc.vn/upload/5c209fe6176b0/2022/04/16/0ca4_family-2.jpg",
-  },
-  { name: "ลักษณะผู้คน", image: placeHolder },
-  {
-    name: "เวลา",
-    image:
-      "https://www.ikea.com/th/en/images/products/pluttis-wall-clock-black__1013114_pe829054_s5.jpg?f=s",
-  },
-  {
-    name: "ประโยค",
-    image:
-      "https://promova.com/content/common_mistakes_in_english_speaking_61f25b56ca.png",
-  },
-  {
-    name: "การทำอาหาร",
-    image:
-      "https://www.health.com/thmb/wfkIOj5dW5H6QLhXqEFHw87ow08=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Health-GettyImages-1477430966-506a007e48744d5488a6ee1c0b729134.jpg",
-  },
-  { name: "ประเภทที่10", image: placeHolder },
-  { name: "ประเภทที่11", image: placeHolder },
-  { name: "ประเภทที่12", image: placeHolder },
-  { name: "ประเภทที่13", image: placeHolder },
-  { name: "ประเภทที่14", image: placeHolder },
-  { name: "ประเภทที่15", image: placeHolder },
-  { name: "ประเภทที่16", image: placeHolder },
-  { name: "ประเภทที่17", image: placeHolder },
-  { name: "ประเภทที่18", image: placeHolder },
-]
+  { name: "อาหาร", image:"https://as1.ftcdn.net/v2/jpg/01/92/00/78/1000_F_192007831_OGdxh37OAqmJpoMuWfgbKKYaQgpa9SJN.jpg" },
+  { name: "สัตว์", image: "https://www.timeforkids.com/wp-content/uploads/2023/11/G3G5_231117_bear_steps.jpg?w=1024" },
+  { name: "สี", image: "https://hips.hearstapps.com/hmg-prod/images/gettyimages-1270527025-6515e6930b257.jpg?crop=1xw:0.84375xh;center,top" },
+  { name: "ร่างกาย", image: "https://media.newyorker.com/photos/5b85b208af438f6d7f78a7a0/master/pass/180910_r32746.jpg" },
+  { name: "ครอบครัว", image:"https://xcdn-cf.vuihoc.vn/upload/5c209fe6176b0/2022/04/16/0ca4_family-2.jpg" },
+  { name: "ลักษณะผู้คน", image: "https://media.newyorker.com/photos/5b85b208af438f6d7f78a7a0/master/pass/180910_r32746.jpg" },
+  { name: "เวลา", image: "https://www.ikea.com/th/en/images/products/pluttis-wall-clock-black__1013114_pe829054_s5.jpg?f=s" },
+  { name: "ประโยค", image: "https://promova.com/content/common_mistakes_in_english_speaking_61f25b56ca.png" },
+  { name: "การทำอาหาร", image: "https://www.health.com/thmb/wfkIOj5dW5H6QLhXqEFHw87ow08=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Health-GettyImages-1477430966-506a007e48744d5488a6ee1c0b729134.jpg" },
+  { name: "โรงเรียน", image: "https://i.pinimg.com/736x/f9/e0/6b/f9e06bde97f3bf383b4f78c63153bbf8.jpg" },
+  { name: "ที่พักอาศัย", image: "https://png.pngtree.com/element_our/sm/20180705/sm_5b3e6b3c55263.jpg" },
+  { name: "การกระทำ", image: "https://img.kapook.com/u/panadda/01aa-600.jpg" },
+  { name: "กฎหมาย", image: "https://static.thairath.co.th/media/dFQROr7oWzulq5Fa5BEdN2JKVPP7SnOdD4lBpUKB87GmmRxmssZW9gTESUgMOFhEUdj.jpg"},
+  { name: "วิชาเรียน", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaVhQzXlPKp_DjqU04nTfu4WRu8spq8VSVQQ&s" },
+  { name: "อาชีพ", image: "https://media.graphassets.com/resize=fit:crop,width:1280,height:660/FYHlVziQ9a9wkVaw4jgw" },
+  { name: "ฤดูกาลและสภาพอากาศ", image: placeHolder },
+  { name: "ลำดับตัวเลข", image: placeHolder },
+  { name: "ธรรมชาติ", image: placeHolder },
+];
 
-const itemsPerPage = 15
+const itemsPerPage = 15; 
 const Category = () => {
-  const [currentPage, setCurrentPage] = useState(1)
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const indexOfLastItem = currentPage * itemsPerPage
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage
-  const currentCategories = allCategories.slice(
-    indexOfFirstItem,
-    indexOfLastItem
-  )
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentCategories = allCategories.slice(indexOfFirstItem, indexOfLastItem);
 
   const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber)
-  }
+    setCurrentPage(pageNumber);
+  };
 
   return (
     <div className="w-screen h-screen flex flex-col justify-between">
@@ -76,11 +46,7 @@ const Category = () => {
         {currentCategories.map((category, index) => (
           <div key={index} className="flex flex-col items-center">
             <Link to={`/category/${category.name}`}>
-              <CatCard
-                image={category.image}
-                title={category.name}
-                className="w-32 h-32 object-cover"
-              />
+              <CatCard image={category.image} title={category.name} className="w-32 h-32 object-cover"/>
             </Link>
           </div>
         ))}
@@ -94,7 +60,7 @@ const Category = () => {
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
--- client/src/pages/SetupForm.jsx
+++ client/src/pages/SetupForm.jsx
@@ -1,61 +1,51 @@
-import React, { useState } from "react"
-import { useNavigate } from "react-router-dom"
-import useFormInput from "../hooks/useFormInput"
-import useFormSubmit from "../hooks/useFormSubmit"
-import Navbar2 from "../components/Navbar2"
-import { Link } from "react-router-dom"
-
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useFormInput from "../hooks/useFormInput";
+import useFormSubmit from "../hooks/useFormSubmit";
+import Navbar2 from "../components/Navbar2";
+import { Link } from "react-router-dom";
+import PathConstants from "../routes/pathConstants";
+// This file is เชื่อมอุปกรณ
 const SetupForm = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const [initialConfig] = useState({
     ip_address: "255.255.255.255",
     port: "14053",
     api_key: "1234",
-  })
+  });
 
-  const { values: config, handleChange } = useFormInput(initialConfig)
+  const { values: config, handleChange } = useFormInput(initialConfig);
 
   const { isSubmitted, handleSubmit } = useFormSubmit(() => {
-    console.log("Form submitted with config:", config)
-    navigate("/control-buttons", { state: { config } })
-  })
+    console.log("Form submitted with config:", config);
+    navigate("/control-buttons", { state: { config } });
+  });
 
   const handleFormSubmit = (event) => {
-    event.preventDefault()
-    handleSubmit()
-  }
+    event.preventDefault();
+    handleSubmit();
+  };
 
-  const handleNewPageNavigation = () => {
-    navigate("/Three-Scene")
-  }
   return (
     <div className="w-screen h-screen flex flex-col justify-between">
       <Navbar2 title="เชื่อมต่ออุปกรณ์" />
-      <div className="flex-grow flex items-center justify-center w-2/3 place-self-center bg-gray-200 my-20 rounded-lg">
-        <div className="p-4 flex flex-col">
+      <div className="flex-grow flex items-center justify-center">
+        <div className="p-4 w-full max-w-md">
           {!isSubmitted ? (
-            <form onSubmit={handleFormSubmit}>
-              {/* <div>
-              <label>เลขที่อยู่ IP (IP address): </label>
-              <input type="text" name="ip_address" value={config.ip_address} onChange={handleChange} required/>
-            </div> 
-            <div>
-              <label>หมายเลขช่องทาง (Port):</label>
-              <input type="text" name="port"value={config.port} onChange={handleChange} required/>
-            </div> 
-            <div>
-              <label>กุญแจ API (API key):</label>
-              <input type="text" ame="api_key" value={config.api_key} onChange={handleChange} required/>
-            </div> */}
+            <form onSubmit={handleFormSubmit} className="flex flex-col gap-4 items-center">
               <label className="form-control w-full max-w-xs">
                 <div className="label">
                   <span className="label-text">เลขที่อยู่ IP (IP address)</span>
                 </div>
                 <input
                   type="text"
-                  placeholder="11.111.111.11"
+                  name="ip_address"
+                  value={config.ip_address}
+                  onChange={handleChange}
+                  placeholder="Type here"
                   className="input input-bordered w-full max-w-xs"
+                  required
                 />
               </label>
 
@@ -65,33 +55,43 @@ const SetupForm = () => {
                 </div>
                 <input
                   type="text"
-                  placeholder="14053"
+                  name="port"
+                  value={config.port}
+                  onChange={handleChange}
+                  placeholder="Type here"
                   className="input input-bordered w-full max-w-xs"
+                  required
                 />
               </label>
-              {/* <label className="form-control w-full max-w-xs">
-              <div className="label">
-                <span className="label-text">กุญแจ API (API key)</span>
-              </div>
-              <input
-                type="text"
-                placeholder="Type here"
-                className="input input-bordered w-full max-w-xs"
-              />
-            </label> */}
-              {/* <button type="submit">ตั้งค่าสำเร็จ</button> */}
+
+              <label className="form-control w-full max-w-xs">
+                <div className="label">
+                  <span className="label-text">กุญแจ API (API key)</span>
+                </div>
+                <input
+                  type="text"
+                  name="api_key"
+                  value={config.api_key}
+                  onChange={handleChange}
+                  placeholder="Type here"
+                  className="input input-bordered w-full max-w-xs"
+                  required
+                />
+              </label>
+
+              <Link
+              to={PathConstants.CATEGORY_ADMIN}
+              className="btn btn-wide btn-lg btn-primary text-primary-content"
+            >
+              ตั้งค่าสำเร็จ
+            </Link>
             </form>
           ) : null}
-          <Link to="/categoryad">
-            <button className="btn btn-wide mt-6 btn-primary text-primary-content">
-              ตั้งค่าสำเร็จ
-            </button>
-          </Link>
-          {/* <button onClick={handleNewPageNavigation}>หน้าแสดงสามมิติ</button> */}
+          
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SetupForm
+export default SetupForm;

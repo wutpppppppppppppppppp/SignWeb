3e71d4238c9ae990dd90cfcdd8d71338ae181ea3
NANAYEMYINTKYI <ayemyint.kyi2004@gmail.com>
2024-07-13 16:43:47 +0700

    update policy

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -3,8 +3,14 @@ import { Link } from "react-router-dom"
 import { FaUser } from "react-icons/fa6"
 import PathConstants from "../routes/pathConstants"
 import { MdOutlineInfo } from "react-icons/md"
-
+import { useState } from "react"
 const LandingPage = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Function to toggle the visibility
+  const toggleVisibility = () => {
+    setIsVisible((prev) => !prev);
+  };
   return (
     <div className="hero min-h-screen">
       <div className="navbar self-start">
@@ -50,51 +56,94 @@ const LandingPage = () => {
               ✕
             </button>
           </form>
-          <h3 className="font-bold text-lg">ข้อตกลงการให้บริการ</h3>
+          {/* <h3 className="font-bold text-lg">ข้อตกลงการให้บริการ</h3>
           <p className="py-5">
           <p>กรุณาอ่านและทำความเข้าใจกฎและข้อตกลงการให้บริการดังต่อไปนี้เพื่อให้การใช้งานเว็บไซต์เป็นไปอย่างราบรื่นและปลอดภัย</p>
 
           <p>ข้อกำหนดการใช้งาน</p>
-
+          
           <p>1. การยอมรับข้อตกลง</p>
           <p>เมื่อคุณเข้าใช้งานเว็บไซต์นี้ แสดงว่าคุณยอมรับที่จะปฏิบัติตามกฎและข้อตกลงที่ระบุไว้ในที่นี้ หากคุณไม่ยอมรับข้อตกลงนี้ กรุณาอย่าเข้าใช้งานเว็บไซต์</p>
 
-          <h3>2. การลงทะเบียนและบัญชีผู้ใช้</h3>
+          <p>2. การลงทะเบียนและบัญชีผู้ใช้</p>
           <p>2.1 คุณต้องลงทะเบียนเพื่อสร้างบัญชีผู้ใช้ก่อนจึงจะสามารถเข้าถึงและดาวน์โหลดไฟล์ท่าภาษามือในรูปแบบ 3D ได้</p>
           <p>2.2 ข้อมูลที่คุณให้มาต้องเป็นข้อมูลที่ถูกต้องและเป็นความจริง</p>
           <p>2.3 คุณมีความรับผิดชอบในการรักษาความปลอดภัยของบัญชีและรหัสผ่านของคุณ</p>
 
-          <h3>3. การใช้งานเนื้อหา</h3>
+          <p>3. การใช้งานเนื้อหา</p>
           <p>3.1 คุณสามารถเข้าถึงและดูท่าภาษามือในรูปแบบ 3D บนเว็บไซต์นี้ได้โดยไม่เสียค่าบริการ</p>
           <p>3.2 หากคุณต้องการดาวน์โหลดไฟล์ท่าภาษามือในรูปแบบ 3D ไปใช้งาน คุณต้องชำระค่าบริการที่กำหนด</p>
           <p>3.3 ห้ามนำไฟล์ที่ดาวน์โหลดไปใช้ในทางที่ละเมิดกฎหมายหรือขัดต่อศีลธรรม</p>
 
-          <h3>4. การชำระค่าบริการ</h3>
+          <p>4. การชำระค่าบริการ</p>
           <p>4.1 ค่าบริการสำหรับการดาวน์โหลดไฟล์ท่าภาษามือในรูปแบบ 3D จะระบุไว้ในเว็บไซต์</p>
           <p>4.2 การชำระเงินสามารถทำได้ผ่านช่องทางที่เว็บไซต์กำหนด</p>
           <p>4.3 ค่าบริการที่ชำระแล้วจะไม่สามารถขอคืนได้</p>
 
-          <h3>5. สิทธิ์ในทรัพย์สินทางปัญญา</h3>
+          <p>5. สิทธิ์ในทรัพย์สินทางปัญญา</p>
           <p>5.1 ไฟล์ท่าภาษามือในรูปแบบ 3D ที่ให้บริการบนเว็บไซต์นี้เป็นทรัพย์สินทางปัญญาของเว็บไซต์</p>
           <p>5.2 คุณไม่ได้รับสิทธิ์ในการขาย แจกจ่าย หรือทำซ้ำไฟล์โดยไม่ได้รับอนุญาต</p>
 
-          <h3>6. ความรับผิดชอบ</h3>
+          <p>6. ความรับผิดชอบ</p>
           <p>6.1 เว็บไซต์ไม่รับผิดชอบต่อความเสียหายหรือความสูญเสียใด ๆ ที่เกิดขึ้นจากการใช้งานเว็บไซต์หรือไฟล์ที่ดาวน์โหลด</p>
           <p>6.2 คุณมีความรับผิดชอบในการใช้งานไฟล์ที่ดาวน์โหลดอย่างถูกต้องตามกฎหมายและจริยธรรม</p>
 
-          <h3>7. การแก้ไขข้อตกลง</h3>
+          <p>7. การแก้ไขข้อตกลง</p>
           <p>เว็บไซต์มีสิทธิ์ในการแก้ไขข้อตกลงนี้ได้ทุกเมื่อโดยไม่ต้องแจ้งให้ทราบล่วงหน้า ข้อตกลงที่แก้ไขจะมีผลบังคับใช้ทันทีที่เผยแพร่บนเว็บไซต์</p>
 
-          <h3>8. ข้อกำหนดทั่วไป</h3>
+          <p>8. ข้อกำหนดทั่วไป</p>
           <p>8.1 ข้อตกลงนี้ถูกกำกับด้วยกฎหมายที่เกี่ยวข้องในประเทศไทย</p>
           <p>8.2 หากข้อตกลงใดในนี้เป็นโมฆะหรือไม่สามารถบังคับใช้ได้ ส่วนที่เหลือของข้อตกลงจะยังคงมีผลบังคับใช้ต่อไป</p>
 
-          <h2>การติดต่อ</h2>
+          <p>การติดต่อ</p>
           <p>หากคุณมีคำถามหรือข้อสงสัยเกี่ยวกับกฎและข้อตกลงการให้บริการ กรุณาติดต่อทีมงานของเราผ่านทางช่องทางที่ระบุไว้ในเว็บไซต์</p>
 
           <p>ขอบคุณที่ใช้บริการเว็บไซต์ของเรา</p>
-
-          </p>
+          </p> */}
+          <div className="relative min-h-screen flex flex-col justify-between">
+            <div className="p-5">
+                  <button
+                    className="btn bg-blue-500 text-white fixed bottom-10 right-10 "
+                    onClick={toggleVisibility}
+                  >
+                    {isVisible ? 'English' : 'ภาษาไทย'}
+                  </button>
+                  {isVisible && (
+                    <p className="mt-4 text-pretty w-full">
+                      <h3>ข้อตกลงในการใช้ซอฟต์แวร์</h3>
+                        <p>ซอฟต์แวร์นี้เป็นผลงานที่พัฒนาขึ้นโดย นาย ปัญญาวุฒิ ปิยะศิรินานันทร์, นางสาว ภรณ์ชนก พิณนุวัตร, นางสาว นาน เอ เมี่ยน จิ จาก มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี
+                        ภายใต้การดูแลของ ดร.จาตุรนต์ หารสมบูรณ์ ภายใต้โครงการ เว็บไซต์เก็บท่าทางสามมิติผู้ใช้ภาษามือ 
+                        คลังคำศัพท์ภาษามือไทยแบบสามมิติ ซึ่งสนับสนุนโดย สำนักงานพัฒนาวิทยาศาสตร์และเทคโนโลยีแห่งชาติ โดยมี
+                        วัตถุประสงค์เพื่อส่งเสริมให้นักเรียนและนักศึกษาได้เรียนรู้และฝึกทักษะในการพัฒนา
+                        ซอฟต์แวร์ ลิขสิทธิ์ของซอฟต์แวร์นี้จึงเป็นของผู้พัฒนา ซึ่งผู้พัฒนาได้อนุญาตให้ส านักงาน
+                        พัฒนาวิทยาศาสตร์และเทคโนโลยีแห่งชาติ เผยแพร่ซอฟต์แวร์นี้ตาม “ต้นฉบับ” โดยไม่มี
+                        การแก้ไขดัดแปลงใด ๆ ทั้งสิ้น ให้แก่บุคคลทั่วไปได้ใช้เพื่อประโยชน์ส่วนบุคคลหรือ
+                        ประโยชน์ทางการศึกษาที่ไม่มีวัตถุประสงค์ในเชิงพาณิชย์ โดยไม่คิดค่าตอบแทนการใช้
+                        ซอฟต์แวร์ ดังนั้น สำนักงานพัฒนาวิทยาศาสตร์และเทคโนโลยีแห่งชาติ จึงไม่มีหน้าที่ใน
+                        การดูแลบำรุงรักษา จัดการอบรมการใช้งาน หรือพัฒนาประสิทธิภาพซอฟต์แวร์ รวมทั้ง
+                        ไม่รับรองความถูกต้องหรือประสิทธิภาพการทำงานของซอฟต์แวร์ ตลอดจนไม่รับประกัน
+                        ความเสียหายต่าง ๆ อันเกิดจากการใช้ซอฟต์แวร์นี้ทั้งสิ้น
+                        </p>
+                    </p>
+                  )}
+                  {!isVisible && (
+                    <p className="mt-4 text-pretty w-full">
+                      <h3>License Agreement</h3>
+                      This software is a work developed by Panyawut Piyasirinanan, Pornchanok Pinnuwat, Nan Aye Myint Kyi from King Mongkut's University of Technology Thonburi under the provision of Jaturon Harnsomburana under Thai Sign Language 3D Gesture Repository, which has been supported by the National Science and Technology
+                      Development Agency (NSTDA), in order to encourage pupils and students to
+                      learn and practice their skills in developing software. Therefore, the intellectual
+                      property of this software shall belong to the developer and the developer gives
+                      NSTDA a permission to distribute this software as an “as is” and non-modified
+                      software for a temporary and non-exclusive use without remuneration to anyone
+                      for his or her own purpose or academic purpose, which are not commercial
+                      purposes. In this connection, NSTDA shall not be responsible to the user for
+                      taking care, maintaining, training, or developing the efficiency of this software.
+                      Moreover, NSTDA shall not be liable for any error, software efficiency and
+                      damages in connection with or arising out of the use of the software.”
+                    </p>
+                  )}
+                </div>
+          </div>
         </div>
       </dialog>
     </div>

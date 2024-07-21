ee9f5662d61ec1a46a9fa0fb508ef81606921439
wutpppppppppppppppppp <gansahy@gmail.com>
2024-07-22 00:55:57 +0700

    changes: merge collections (category, vocab) to 1, rewrite schema

--- help/routes/api/categories/index.js
+++ help/routes/api/categories/index.js
@@ -6,9 +6,26 @@ const categorySchema = {
     response: {
       200: S.array().items(
         S.object()
+          .prop("_id", S.string())
           .prop("name", S.string())
           .prop("description", S.string())
           .prop("picture", S.string())
+          .prop(
+            "vocabularies",
+            S.array()
+              .items(
+                S.object()
+                  .prop("_id", S.string())
+                  .prop("name", S.string())
+                  .prop("description", S.string())
+                  .prop("picture", S.string())
+                  .prop("created_at", S.string().format("date-time"))
+                  .prop("updated_at", S.string().format("date-time"))
+              )
+              .default([])
+          )
+          .prop("created_at", S.string().format("date-time"))
+          .prop("updated_at", S.string().format("date-time"))
       ),
     },
   },
@@ -19,12 +36,38 @@ const addCategorySchema = {
     body: S.object()
       .prop("name", S.string().required())
       .prop("description", S.string().required())
-      .prop("picture", S.string().required()),
+      .prop("picture", S.string().required())
+      .prop(
+        "vocabularies",
+        S.array()
+          .items(
+            S.object()
+              .prop("name", S.string().required())
+              .prop("description", S.string().required())
+              .prop("picture", S.string().required())
+              .prop("created_at", S.string().format("date-time"))
+              .prop("updated_at", S.string().format("date-time"))
+          )
+          .default([])
+      ),
     response: {
       201: S.object()
+        .prop("_id", S.string())
         .prop("name", S.string())
         .prop("description", S.string())
         .prop("picture", S.string())
+        .prop(
+          "vocabularies",
+          S.array().items(
+            S.object()
+              .prop("_id", S.string())
+              .prop("name", S.string())
+              .prop("description", S.string())
+              .prop("picture", S.string())
+              .prop("created_at", S.string().format("date-time"))
+              .prop("updated_at", S.string().format("date-time"))
+          )
+        )
         .prop("created_at", S.string().format("date-time"))
         .prop("updated_at", S.string().format("date-time")),
     },
@@ -32,6 +75,7 @@ const addCategorySchema = {
 };
 
 async function categoriesRoutes(fastify) {
+  // GET /api/categories route
   fastify.get("/", categorySchema, async function (request, reply) {
     try {
       const categoriesCollection = fastify.mongo.client
@@ -46,23 +90,32 @@ async function categoriesRoutes(fastify) {
     }
   });
 
+  // POST /api/categories route
   fastify.post("/", addCategorySchema, async function (request, reply) {
     try {
-      const { name, description, picture } = request.body;
+      const { name, description, picture, vocabularies = [] } = request.body;
       const categoriesCollection = fastify.mongo.client
         .db("sample_sign")
         .collection("categories");
 
-      const currentTime = new Date();
+      const currentTime = new Date().toISOString();
       const newCategory = {
         name,
         description,
         picture,
+        vocabularies: vocabularies.map((vocab) => ({
+          ...vocab,
+          _id: new fastify.mongo.ObjectId(),
+          created_at: new Date().toISOString(),
+          updated_at: new Date().toISOString(),
+        })),
         created_at: currentTime,
         updated_at: currentTime,
       };
 
-      await categoriesCollection.insertOne(newCategory);
+      const result = await categoriesCollection.insertOne(newCategory);
+      newCategory._id = result.insertedId;
+
       fastify.log.info(`Added new category: ${JSON.stringify(newCategory)}`);
       reply.code(201).send(newCategory);
     } catch (err) {
@@ -72,6 +125,11 @@ async function categoriesRoutes(fastify) {
   });
 }
 
-export default fp(async function (app, opts) {
-  app.register(categoriesRoutes, { prefix: "/api/categories" });
-});
+export default fp(
+  async function (app, opts) {
+    app.register(categoriesRoutes, { prefix: "/api/categories" });
+  },
+  {
+    name: "category-routes",
+  }
+);

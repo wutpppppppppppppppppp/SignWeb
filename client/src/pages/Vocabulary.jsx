825263a3ede747127cf7b8034f11c407f75ba176
wutpppppppppppppppppp <gansahy@gmail.com>
2024-07-22 19:09:46 +0700

    fix: photo not showing

--- client/src/pages/Vocabulary.jsx
+++ client/src/pages/Vocabulary.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react"
-import { useParams } from "react-router-dom"
+import { useParams, Link } from "react-router-dom"
 import axios from "axios"
 import Navbar from "../components/Navbar"
-
+import SearchBox from "../components/SearchBox"
 const Vocabulary = () => {
   const { category } = useParams()
   const [vocabularies, setVocabularies] = useState([])
@@ -30,12 +30,12 @@ const Vocabulary = () => {
 
   return (
     <div className="w-screen h-screen">
-      <Navbar title={`คำศัพท์${categoryName}`} />
+      <Navbar title={`คำศัพท์${category}`} />
       <div className="hero w-screen bg-[url('https://as1.ftcdn.net/v2/jpg/01/92/00/78/1000_F_192007831_OGdxh37OAqmJpoMuWfgbKKYaQgpa9SJN.jpg')]">
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-neutral-content text-center">
           <div className="max-w-md mx-auto">
-            <h1 className="mb-5 text-5xl font-bold">{categoryName}</h1>
+            <h1 className="mb-5 text-5xl font-bold">{category}</h1>
             <SearchBox placeholder={"ค้นหาคำศัพท์ ..."} />
           </div>
         </div>
@@ -44,7 +44,7 @@ const Vocabulary = () => {
       <div className="p-4">
         <div className="grid grid-cols-4 gap-4 justify-center">
           {vocabularies.map((vocab, index) => (
-            <Link key={index} to={`/category/${categoryName}/${vocab.name}`}>
+            <Link key={index} to={`/category/${category}/${vocab.name}`}>
               <div className="border p-4 flex flex-col items-center">
                 <b>{vocab.name}</b>
                 {/* <div className="bg-cover bg-center" style="background-image: url(&)"></div> */}
@@ -58,4 +58,4 @@ const Vocabulary = () => {
   )
 }
 
-export default Vocab
+export default Vocabulary

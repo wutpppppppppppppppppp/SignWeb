6a886c5344fe4e3d8b39ed5899e94d17dcf5dbf3
wutpppppppppppppppppp <gansahy@gmail.com>
2024-07-22 18:06:39 +0700

    delete: server, server2 and rename help to server

--- server/models/mocapModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const mocapSchema = new mongoose.Schema({
-  vocabulary: { type: mongoose.Schema.Types.ObjectId, ref: 'Vocabulary' },
-  data: String,
-  createdAt: { type: Date, default: Date.now },
-});
-
-export default mongoose.model('Mocap', mocapSchema);

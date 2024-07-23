da1da9a2cde7e852247ba080e0825a7198bc88a6
wutpppppppppppppppppp <gansahy@gmail.com>
2024-07-23 22:34:13 +0700

    fix: category api picture cdn

--- server/routes/api/vocabularies/index.js
+++ server/routes/api/vocabularies/index.js
@@ -2,6 +2,7 @@ import fp from "fastify-plugin";
 import S from "fluent-json-schema";
 import cloudinary from "../../../config/cloudinary.js";
 import stream from "stream";
+
 const vocabulariesSchema = {
   schema: {
     querystring: S.object().prop("category", S.string().required()),
@@ -25,7 +26,7 @@ const addVocabularySchema = {
       .prop("category_id", S.string().required())
       .prop("name", S.string().required())
       .prop("description", S.string())
-      .prop("picture", S.string()), // No need for contentEncoding as we'll handle it differently
+      .prop("picture", S.string()),
     response: {
       201: S.object()
         .prop("_id", S.string().format("uuid"))
@@ -38,6 +39,23 @@ const addVocabularySchema = {
   },
 };
 
+const updateVocabularySchema = {
+  schema: {
+    body: S.object()
+      .prop("name", S.string())
+      .prop("description", S.string())
+      .prop("picture", S.string()),
+    response: {
+      200: S.object()
+        .prop("_id", S.string())
+        .prop("name", S.string())
+        .prop("description", S.string())
+        .prop("picture", S.string())
+        .prop("created_at", S.string().format("date-time"))
+        .prop("updated_at", S.string().format("date-time")),
+    },
+  },
+};
 const threedSchema = {
   schema: {
     querystring: S.object().prop("vocabulary_id", S.string().required()),
@@ -64,24 +82,6 @@ const addThreedSchema = {
         .prop("_id", S.string().format("uuid"))
         .prop("vocabulary_id", S.string().format("uuid"))
         .prop("three_dim_data", S.object()) // Adjust based on the actual structure
-        .prop("created_at", S.string().format("date-time"))
-        .prop("updated_at", S.string().format("date-time")),
-    },
-  },
-};
-
-const updateVocabularySchema = {
-  schema: {
-    body: S.object()
-      .prop("name", S.string())
-      .prop("description", S.string())
-      .prop("picture", S.string()),
-    response: {
-      200: S.object()
-        .prop("_id", S.string())
-        .prop("name", S.string())
-        .prop("description", S.string())
-        .prop("picture", S.string())
         .prop("created_at", S.string().format("date-time"))
         .prop("updated_at", S.string().format("date-time")),
     },
@@ -120,47 +120,17 @@ async function vocabulariesRoutes(fastify) {
     }
   });
 
-  fastify.get("/3d", threedSchema, async function (request, reply) {
-    try {
-      const { vocabulary_id } = request.query;
-      const threedCollection = fastify.mongo.client
-        .db("sample_sign")
-        .collection("threed");
-
-      const threedData = await threedCollection
-        .find({ vocabulary_id: new fastify.mongo.ObjectId(vocabulary_id) })
-        .toArray();
-
-      if (threedData.length === 0) {
-        fastify.log.warn(
-          `No 3D data found for vocabulary ID: ${vocabulary_id}`
-        );
-        reply
-          .code(404)
-          .send({ error: "No 3D data found for this vocabulary ID" });
-      } else {
-        fastify.log.info(`Fetched 3D data: ${JSON.stringify(threedData)}`);
-        reply.send(threedData);
-      }
-    } catch (err) {
-      fastify.log.error(err, "Failed to fetch 3D data");
-      reply.code(500).send({ error: "Failed to fetch 3D data" });
-    }
-  });
-
   fastify.post(
     "/",
-    {
-      schema: addVocabularySchema,
-    },
+    { schema: addVocabularySchema },
     async function (request, reply) {
       try {
         const { category_id, name, description } = request.body;
 
         // Access file directly from request.body
         const pictureBuffer = request.body.picture;
-        console.log(request.body.picture);
-        console.log(pictureBuffer);
+        // console.log(request.body.picture);
+        // console.log(pictureBuffer);
         if (!pictureBuffer) {
           reply.code(400).send({ error: "Picture file is required" });
           return;
@@ -223,6 +193,33 @@ async function vocabulariesRoutes(fastify) {
     }
   );
 
+  fastify.get("/3d", threedSchema, async function (request, reply) {
+    try {
+      const { vocabulary_id } = request.query;
+      const threedCollection = fastify.mongo.client
+        .db("sample_sign")
+        .collection("threed");
+
+      const threedData = await threedCollection
+        .find({ vocabulary_id: new fastify.mongo.ObjectId(vocabulary_id) })
+        .toArray();
+
+      if (threedData.length === 0) {
+        fastify.log.warn(
+          `No 3D data found for vocabulary ID: ${vocabulary_id}`
+        );
+        reply
+          .code(404)
+          .send({ error: "No 3D data found for this vocabulary ID" });
+      } else {
+        fastify.log.info(`Fetched 3D data: ${JSON.stringify(threedData)}`);
+        reply.send(threedData);
+      }
+    } catch (err) {
+      fastify.log.error(err, "Failed to fetch 3D data");
+      reply.code(500).send({ error: "Failed to fetch 3D data" });
+    }
+  });
   fastify.post("/3d", addThreedSchema, async function (request, reply) {
     try {
       const { vocabulary_id, three_dim_data } = request.body;

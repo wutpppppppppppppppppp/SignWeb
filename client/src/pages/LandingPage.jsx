23520c3e9c1973a7f4b1b8962ffc0f412b29d79f
wutpppppppppppppppppp <gansahy@hotmail.com>
2024-07-25 11:38:30 +0700

    change: landing page design

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -33,7 +33,7 @@ const LandingPage = () => {
 
       <div className="hero-content text-center">
         <div className="max-w-fit">
-          <p className="text-8xl mb-10">คลังเก็บภาษามือไทย</p>
+          <h1 className="text-8xl mb-10">คลังเก็บภาษามือไทย</h1>
           <Link
             to={PathConstants.CATEGORY}
             className="btn btn-wide btn-lg btn-primary text-primary-content"
@@ -43,9 +43,9 @@ const LandingPage = () => {
         </div>
       </div>
 
-      <div className="Term and conditions place-self-end size-20">
+      <div className="place-self-end size-20">
         <MdOutlineInfo
-          className="size-8 cursor-pointer"
+          className="size-8 cursor-pointer hover:text-info"
           title="ข้อตกลงการให้บริการ"
           onClick={() => document.getElementById("my_modal_3").showModal()}
         />
@@ -53,7 +53,7 @@ const LandingPage = () => {
       <dialog id="my_modal_3" className="modal">
         <div className="modal-box">
           <form method="dialog">
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 hover:text-error">
               ✕
             </button>
           </form>
@@ -102,12 +102,41 @@ const LandingPage = () => {
           <p>ขอบคุณที่ใช้บริการเว็บไซต์ของเรา</p>
           </p> */}
           <div className="relative flex justify-between">
-            <div className="m-5">
+            <div className="flex flex-col">
               {isVisible ? (
+                <div>
+                  <h3 className="pb-2">License Agreement</h3>
+                  <p>
+                    This software is a work developed by Panyawut Piyasirinanan,
+                    Pornchanok Pinnuwat, Nan Aye Myint Kyi from King Mongkut's
+                    University of Technology Thonburi under the provision of
+                    Jaturon Harnsomburana under Thai Sign Language 3D Gesture
+                    Repository, which has been supported by the National Science
+                    and Technology Development Agency (NSTDA), in order to
+                    encourage pupils and students to learn and practice their
+                    skills in developing software.
+                    <br />
+                    <br />
+                    Therefore, the intellectual property of this software shall
+                    belong to the developer and the developer gives NSTDA a
+                    permission to distribute this software as an “as is” and
+                    non-modified software for a temporary and non-exclusive use
+                    without remuneration to anyone for his or her own purpose or
+                    academic purpose, which are not commercial purposes.
+                    <br />
+                    <br />
+                    In this connection, NSTDA shall not be responsible to the
+                    user for taking care, maintaining, training, or developing
+                    the efficiency of this software. Moreover, NSTDA shall not
+                    be liable for any error, software efficiency and damages in
+                    connection with or arising out of the use of the software.
+                  </p>
+                </div>
+              ) : (
                 <div>
                   <h3 className="pb-2">ข้อตกลงในการใช้ซอฟต์แวร์</h3>
                   <p>
-                    &emsp;ซอฟต์แวร์นี้เป็นผลงานที่พัฒนาขึ้นโดย นาย ปัญญาวุฒิ
+                    ซอฟต์แวร์นี้เป็นผลงานที่พัฒนาขึ้นโดย นาย ปัญญาวุฒิ
                     ปิยะศิรินานันทร์, นางสาว ภรณ์ชนก พิณนุวัตร, นางสาว นาน เอ
                     เมี่ยน จิ จาก มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี
                     ภายใต้การดูแลของ ดร.จาตุรนต์ หารสมบูรณ์ ภายใต้โครงการ
@@ -116,7 +145,6 @@ const LandingPage = () => {
                     สำนักงานพัฒนาวิทยาศาสตร์และเทคโนโลยีแห่งชาติ โดยมี
                     <br />
                     <br />
-                    &emsp;
                     วัตถุประสงค์เพื่อส่งเสริมให้นักเรียนและนักศึกษาได้เรียนรู้และฝึกทักษะในการพัฒนา
                     ซอฟต์แวร์ ลิขสิทธิ์ของซอฟต์แวร์นี้จึงเป็นของผู้พัฒนา
                     ซึ่งผู้พัฒนาได้อนุญาตให้สำนักงาน
@@ -124,9 +152,10 @@ const LandingPage = () => {
                     “ต้นฉบับ” โดยไม่มี การแก้ไขดัดแปลงใด ๆ ทั้งสิ้น
                     ให้แก่บุคคลทั่วไปได้ใช้เพื่อประโยชน์ส่วนบุคคลหรือ
                     ประโยชน์ทางการศึกษาที่ไม่มีวัตถุประสงค์ในเชิงพาณิชย์
-                    โดยไม่คิดค่าตอบแทนการใช้ ซอฟต์แวร์ <br />
+                    โดยไม่คิดค่าตอบแทนการใช้ ซอฟต์แวร์
                     <br />
-                    &emsp; ดังนั้น สำนักงานพัฒนาวิทยาศาสตร์และเทคโนโลยีแห่งชาติ
+                    <br />
+                    ดังนั้น สำนักงานพัฒนาวิทยาศาสตร์และเทคโนโลยีแห่งชาติ
                     จึงไม่มีหน้าที่ใน การดูแลบำรุงรักษา จัดการอบรมการใช้งาน
                     หรือพัฒนาประสิทธิภาพซอฟต์แวร์ รวมทั้ง
                     ไม่รับรองความถูกต้องหรือประสิทธิภาพการทำงานของซอฟต์แวร์
@@ -134,50 +163,20 @@ const LandingPage = () => {
                     อันเกิดจากการใช้ซอฟต์แวร์นี้ทั้งสิ้น
                   </p>
                 </div>
-              ) : (
-                <div>
-                  <h3 className="pb-2">License Agreement</h3>
-                  <p>
-                    &emsp;This software is a work developed by Panyawut
-                    Piyasirinanan, Pornchanok Pinnuwat, Nan Aye Myint Kyi from
-                    King Mongkut's University of Technology Thonburi under the
-                    provision of Jaturon Harnsomburana under Thai Sign Language
-                    3D Gesture Repository, which has been supported by the
-                    National Science and Technology Development Agency (NSTDA),
-                    in order to encourage pupils and students to learn and
-                    practice their skills in developing software.
-                    <br />
-                    <br />
-                    &emsp; Therefore, the intellectual property of this software
-                    shall belong to the developer and the developer gives NSTDA
-                    a permission to distribute this software as an “as is” and
-                    non-modified software for a temporary and non-exclusive use
-                    without remuneration to anyone for his or her own purpose or
-                    academic purpose, which are not commercial purposes.
-                    <br />
-                    <br />
-                    &emsp;In this connection, NSTDA shall not be responsible to
-                    the user for taking care, maintaining, training, or
-                    developing the efficiency of this software. Moreover, NSTDA
-                    shall not be liable for any error, software efficiency and
-                    damages in connection with or arising out of the use of the
-                    software.
-                  </p>
-                </div>
               )}
-              <label className="swap absolute bottom-0 right-0 text-neutral-content">
+              <label className="swap text-neutral-content self-end hover:text-accent">
                 <input
                   type="checkbox"
                   checked={isVisible}
                   onChange={toggleVisibility}
                 />
                 <div className="swap-on flex gap-1">
+                  <span className="flex-1 text-end">ไทย</span>
                   <TbWorld className="h-full" />
-                  <span>ไทย</span>
                 </div>
-                <div className="swap-off flex gap-1">
-                  <TbWorld className="h-full" />
+                <div className="swap-off flex gap-1 hover:text-info">
                   <span>English</span>
+                  <TbWorld className="h-full" />
                 </div>
               </label>
             </div>

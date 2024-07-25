65984f0d866efb76f798ec24f7e7ae3d665df93c
KwinyarutP <kwinyarut2546@gmail.com>
2024-07-25 17:35:41 +0200

    change: pull

--- client/src/components/CatCard.jsx
+++ client/src/components/CatCard.jsx
@@ -2,14 +2,13 @@ import React from "react"
 
 const CatCard = ({ image, title }) => {
   return (
-    <div className="card bg-primary shadow-lg hover:shadow-xl transition-shadow duration-300 w-full max-w-xs">
+    <div className="card bg-lightergray shadow-lg hover:shadow-xl transition-shadow duration-300 w-full max-w-xs">
       <figure className="px-4 pt-4">
         <img src={image} alt={title} className="rounded-xl object-cover w-full aspect-square" />
       </figure>
       <div className="card-body p-4 text-center">
         <h3 className="card-title text-primary-content justify-center text-lg">{title}</h3>
       </div>
-   
     </div>
   );
 }

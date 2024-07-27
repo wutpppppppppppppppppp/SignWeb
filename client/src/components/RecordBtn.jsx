28e15809178e3305d88b1bb5ae5ba965ddc3b7b2
KwinyarutP <kwinyarut2546@gmail.com>
2024-07-27 11:47:48 +0200

    edit: change RecordBtn2

--- client/src/components/RecordBtn.jsx
+++ client/src/components/RecordBtn.jsx
@@ -4,8 +4,6 @@ const RecordButton = ({
   isRecording,
   handleStartRecording,
   handleStopRecording,
-  className,
-  style,
 }) => {
   const handleToggle = () => {
     if (isRecording) {
@@ -17,10 +15,9 @@ const RecordButton = ({
 
   return (
     <div
-      className={`flex justify-center items-center cursor-pointer transition-all duration-300 ease-in-out ${
-        isRecording ? "bg-white" : ""
-      } ${className}`}
-      style={style}
+    className={`flex justify-center items-center w-20 h-20 cursor-pointer transition-all duration-300 ease-in-out ${
+      isRecording ? "bg-white" : ""
+    }`}
       onClick={handleToggle}
     >
       <div
@@ -30,7 +27,7 @@ const RecordButton = ({
       >
         <div
           className={`${
-            isRecording ? "w-3/5 h-3/5 rounded-md" : "w-4/5 h-4/5 rounded-full"
+            isRecording ? "w-10 h-10 rounded-md" : "w-14 h-14 rounded-full"
           } bg-red-600 transition-all duration-300 ease-in-out`}
         ></div>
       </div>

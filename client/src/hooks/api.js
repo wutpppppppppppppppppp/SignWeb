8b7d127f5a3bf41d0686c75568885d011dd3d7bb
wutpppppppppppppppppp <gansahy@gmail.com>
2024-07-27 12:37:10 +0700

    fix: timeout

--- client/src/hooks/api.js
+++ client/src/hooks/api.js
@@ -2,9 +2,20 @@ import axios from "axios"
 
 const api = axios.create({
   baseURL: "http://localhost:3000",
-  timeout: 1000,
-  headers: { "X-Custom-Header": "foobar" },
+  timeout: 3000,
+  // headers: { "X-Custom-Header": "foobar" },
 })
+
+// Add a response interceptor
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    // Handle error globally
+    console.error("API call error:", error)
+    return Promise.reject(error)
+  }
+)
+
 export default api
 
 // const temp = useMemo(() => {
--- server/plugins/db.js
+++ server/plugins/db.js
@@ -5,7 +5,7 @@ import fp from "fastify-plugin";
 import mongodb from "@fastify/mongodb";
 import dotenv from "dotenv";
 
-dotenv.config({ path: "./help/.env" });
+dotenv.config({ path: "./server/.env" });
 
 export default fp(async (fastify, opts) => {
   const connectionString = process.env.CONNECTION_STRING.replace(
